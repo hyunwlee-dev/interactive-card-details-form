9735d6f057a80750437052a59284506a42e5dc49
hyunwlee <hwlee3928@gmail.com>
2024-03-23 00:54:06 +0900

    style: background image depends on $breakpoints

--- app/styles/global.css.ts
+++ app/styles/global.css.ts
@@ -1,16 +1,33 @@
 import { globalStyle } from "@vanilla-extract/css";
+import { $breakpoints } from "./utils";
 
 globalStyle('*, *::before, *::after', {
   boxSizing: "border-box",
   padding: "0",
   margin: "0",
-  overflowX: "hidden"
 });
 
 globalStyle('html, body', {
   minHeight: '100vh',
   fontSize: '18px',
   fontWeight: '500',
+});
+
+globalStyle('body::before', {
+  position: "absolute",
+  content: '',
+  width: "100%",
+  height: "240px",
+  background: "url('/images/bg-main-mobile.png') no-repeat center / 100% 100%",
+  inset: "0",
+  zIndex: "-100",
+  '@media': {
+    [$breakpoints.laptop]: {
+      width: "483px",
+      height: "100%",
+      background: "url('/images/bg-main-desktop.png') no-repeat center / 100% 100%"
+    },
+  }
 });
 
 globalStyle('a', {
--- app/styles/utils.ts
+++ app/styles/utils.ts
@@ -1,5 +1,6 @@
 export const $breakpoints = {
   mobile: 'screen and (min-width: 375px)',
+  laptop: 'screen and (min-width: 1024px)',
   desktop: 'screen and (min-width: 1440px)'
 } as const
 
